3782e69570716a222eb8c552e38b98ab6f5a7f5d
Vick <eapvick@gmail.com>
2016-10-02 13:36:18 -0700

    downloaded images on index and made it local

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
 var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var cache = require("gulp-cache");
 
+gulp.task("images", function() {
+   return gulp.src("img/*")
+   // Caching images that ran through imagemin
+            .pipe(cache(imagemin()))
+            .pipe(gulp.dest("dist/img"))
+
+});
 
