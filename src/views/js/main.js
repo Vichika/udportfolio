c45f80d1ca5a05ea45ffc8b3905e80ed4e535ab4
Vick <eapvick@gmail.com>
2016-10-08 18:50:05 -0700

    attempting  get speed of resize pizza under 5 ms

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -416,15 +416,16 @@ var resizePizzas = function (size) {
 
     // Changes the value for the size of the pizza above the slider
     function changeSliderLabel(size) {
+        var pizzaSize = document.getElementById("pizzaSize");
         switch (size) {
             case "1":
-                document.querySelector("#pizzaSize").innerHTML = "Small";
+                pizzaSize.innerHTML = "Small";
                 return;
             case "2":
-                document.querySelector("#pizzaSize").innerHTML = "Medium";
+                pizzaSize.innerHTML = "Medium";
                 return;
             case "3":
-                document.querySelector("#pizzaSize").innerHTML = "Large";
+                pizzaSize.innerHTML = "Large";
                 return;
             default:
                 console.log("bug in changeSliderLabel");
